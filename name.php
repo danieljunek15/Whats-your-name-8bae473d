dc14203834320ca638d5757c5c1b829b465281d2
danieljunek15 <dvandenbrink@roc-dev.com>
2019-10-03 11:07:41 +0200

    Uitwerking van de exercise

---
+++ name.php
@@ -0,0 +1,6 @@
+<?php
+echo "gello whats name u?" .PHP_EOL;
+$name=readline();
+echo "good now me now u first name can me have second one? pls" .PHP_EOL;
+$secondName=readline();
+echo "ur name is: " . "$name"." "."$secondName"; 
